fb3b0f4779b973382451a5436e4b2511545b21cf
jacksonAbrao <jackson.comp1681@hotmail.com>
2022-04-16 23:42:49 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,39 @@
+const header = document.querySelector('header');
+/* --------------- Grab elements from DOM --------------- */
+
+/* --------------- Sticky Navbar --------------- */
+function stickyNavbar() {
+    header.classList.toggle('scrolled', window.pageYOffset > 0);
+}
+
+stickyNavbar();
+
+window.addEventListener('scroll', stickyNavbar);
+
+/* --------------- Reveal Animation --------------- */
+
+let sr = ScrollReveal({
+    duration: 2500,
+    distance: '60px',
+});
+
+sr.reveal(".showcase-info", { delay: 600 });
+sr.reveal(".showcase-image", { origin: "top", delay: 700 });
+
+/* --------------- Skills Progress Bar Animation --------------- */
+
+/* --------------- Services Counter Animation --------------- */
+
+/* --------------- Portfolio Filter Animation --------------- */
+
+/* --------------- Modal Pop Up Animation Animation --------------- */
+
+/* --------------- Modal Pop Up Animation Animation --------------- */
+
+/* --------------- Change Active Link On Scroll --------------- */
+
+/* --------------- Change Page Theme --------------- */
+
+/* --------------- Open & Close Navbar Menu --------------- */
+
+
